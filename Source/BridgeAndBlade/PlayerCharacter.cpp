b11c0d153445789d81cd5afb3a1ed570aa2d165f
Farhan-Mehmood <farhanmehmood010@gmail.com>
2025-12-02 13:57:33 +0000

    Making Player Character

---
+++ Source/BridgeAndBlade/PlayerCharacter.cpp
@@ -0,0 +1,94 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerCharacter.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+APlayerCharacter::APlayerCharacter()
+{
+ 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootScene = CreateDefaultSubobject<USceneComponent>(TEXT("RootScene"));
+	RootComponent = RootScene;
+
+	PlayerMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PlayerMesh"));
+	PlayerMesh->SetupAttachment(RootComponent);
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(PlayerMesh);
+	Camera->bUsePawnControlRotation = true;
+
+
+	AutoPossessPlayer = EAutoReceiveInput::Player0;
+
+}
+
+// Called when the game starts or when spawned
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	APlayerController* PC = Cast<APlayerController>(GetController());
+	if (PC)
+	{
+		UEnhancedInputLocalPlayerSubsystem* Subsystem =
+			ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PC->GetLocalPlayer());
+
+		if (Subsystem)
+		{
+			Subsystem->AddMappingContext(DefaultMappingContext, 0);
+		}
+	}
+	
+}
+
+// Called every frame
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EIC = Cast<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EIC->BindAction(MoveUpAction, ETriggerEvent::Triggered, this, &APlayerCharacter::MoveUp);
+		EIC->BindAction(MoveRightAction, ETriggerEvent::Triggered, this, &APlayerCharacter::MoveRight);
+		EIC->BindAction(ZoomCameraAction, ETriggerEvent::Triggered, this, &APlayerCharacter::ZoomCamera);
+	}
+
+
+}
+
+
+// Movement input handlers
+void APlayerCharacter::MoveUp(const FInputActionValue& Value)
+{
+	AddMovementInput(GetActorForwardVector() * Value.Get<float>());
+}
+
+void APlayerCharacter::MoveRight(const FInputActionValue& Value)
+{
+	AddMovementInput(GetActorRightVector() * Value.Get<float>());
+}
+
+void APlayerCharacter::ZoomCamera(const FInputActionValue& Value)
+{
+	// Simple zoom by moving camera along its local X axis. Tune ZoomSpeed as needed.
+	const float AxisValue = Value.Get<float>();
+	const float ZoomSpeed = 20.0f;
+
+	if (Camera && FMath::Abs(AxisValue) > KINDA_SMALL_NUMBER)
+	{
+		Camera->AddLocalOffset(FVector(AxisValue * ZoomSpeed, 0.f, 0.f));
+	}
+}
+
---
+++ Source/BridgeAndBlade/PlayerCharacter.h
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+#include "PlayerCharacter.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+class USceneComponent;
+class UCameraComponent;
+
+UCLASS()
+class BRIDGEANDBLADE_API APlayerCharacter : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	APlayerCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(VisibleAnywhere)
+	UStaticMeshComponent* PlayerMesh;
+
+	UPROPERTY(EditAnywhere, Category = "Components")
+	USceneComponent* RootScene;
+
+	UPROPERTY(EditAnywhere, Category = "Components")
+	UCameraComponent* Camera;
+
+	// Input
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputMappingContext* DefaultMappingContext;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* MoveUpAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* MoveRightAction;
+
+	UPROPERTY(EditAnywhere, Category = "Input")
+	UInputAction* ZoomCameraAction;
+
+	// Movement input handlers
+	void MoveUp(const FInputActionValue& Value);
+	void MoveRight(const FInputActionValue& Value);
+	void ZoomCamera(const FInputActionValue& Value);
+};
