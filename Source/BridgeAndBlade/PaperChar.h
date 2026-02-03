e64e8ab9330b1d10a31db7861ec1766b05e3d922
NShot3 <141463296+NShot3@users.noreply.github.com>
2026-02-03 11:46:17 +0000

    Revert "Merge branch 'master' of https://github.com/Farhan-Mehmood/BridgeAndBlade"
    This reverts commit 6a0bfc1db370ffeb3838e2de64d10b41d819118a, reversing
    changes made to 376e0cee8b820839e5885bd05d639f9895cec008.

--- Source/BridgeAndBlade/PaperChar.h
+++ Source/BridgeAndBlade/PaperChar.h
@@ -1,8 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+
 #pragma once
 
 #include "PaperBase.h"
+#include "WeaponBase.h" 
 #include "CoreMinimal.h"
 #include "PaperCharacter.h"
 #include "GameFramework/CharacterMovementComponent.h"
@@ -11,52 +13,106 @@
 
 class UInputMappingContext;
 class UInputAction;
-//class USceneComponent;
 class UCameraComponent;
-//class UFloatingPawnMovement;
+class UDataTable;
 
 UCLASS()
 class BRIDGEANDBLADE_API APaperChar : public APaperBase
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	// Sets default values for this pawn's properties
-	APaperChar();
-	
+    APaperChar();
+    
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    virtual void BeginPlay() override;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Database")
+    UDataTable* ItemDataTable;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+    FVector2D FacingDirection;
+
+    void UpdateWeaponRotation();
 
 public:
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    virtual void Tick(float DeltaTime) override;
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
-	//UPROPERTY(EditAnywhere, Category = "Components")
-	//USceneComponent* RootScene;
+    UPROPERTY(EditAnywhere, Category = "Components")
+    UCameraComponent* Camera;
 
-	UPROPERTY(EditAnywhere, Category = "Components")
-	UCameraComponent* Camera;
 
-	//UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
-	//UFloatingPawnMovement* MovementComponent;
+    // Input
+    UPROPERTY(EditAnywhere, Category = "Input")
+    UInputMappingContext* DefaultMappingContext;
 
-	// Input
-	UPROPERTY(EditAnywhere, Category = "Input")
-	UInputMappingContext* DefaultMappingContext;
+    UPROPERTY(EditAnywhere, Category = "Input")
+    UInputAction* MoveUpAction;
 
-	UPROPERTY(EditAnywhere, Category = "Input")
-	UInputAction* MoveUpAction;
+    UPROPERTY(EditAnywhere, Category = "Input")
+    UInputAction* MoveRightAction;
 
-	UPROPERTY(EditAnywhere, Category = "Input")
-	UInputAction* MoveRightAction;
+    UPROPERTY(EditAnywhere, Category = "Input")
+    UInputAction* AttackAction;
 
-	UPROPERTY(EditAnywhere, Category = "Input")
-	UInputAction* ZoomCameraAction;
+    UPROPERTY(EditAnywhere, Category = "Input")
+    UInputAction* ZoomCameraAction;
 
-	// Movement input handlers
-	void MoveUp(const FInputActionValue& Value);
-	void MoveRight(const FInputActionValue& Value);
-	void ZoomCamera(const FInputActionValue& Value);
+
+    // Weapon positioning
+    UPROPERTY(EditDefaultsOnly, Category = "Weapon")
+    FVector WeaponRelativeLocation;
+
+    UPROPERTY(EditDefaultsOnly, Category = "Weapon")
+    FRotator WeaponRelativeRotation;
+
+
+    // Inventory
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Weapon")
+    AWeaponBase* EquippedWeapon;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Inventory")
+    TArray<FName> WeaponInventory;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = "Inventory")
+    TMap<FName, int32> MaterialInventory;
+
+
+    // Inventory functions
+    UFUNCTION(BlueprintCallable, Category = "Inventory")
+    void AddItemToInventory(FName ItemName, int32 Amount = 1);
+
+    UFUNCTION(BlueprintCallable, Category = "Inventory")
+    void EquipWeapon(int32 InventoryIndex);
+
+    UFUNCTION(BlueprintCallable, Category = "Inventory")
+    void UnequipWeapon();
+
+    UFUNCTION(BlueprintCallable, Category = "Inventory")
+    int32 GetItemCount(FName ItemName) const;
+
+    UFUNCTION(BlueprintCallable, Category = "Inventory")
+    bool HasItem(FName ItemName, int32 Amount = 1) const;
+
+    UFUNCTION(BlueprintCallable, Category = "Inventory")
+    bool RemoveItem(FName ItemName, int32 Amount = 1);
+
+    // Crafting
+    UFUNCTION(BlueprintCallable, Category = "Crafting")
+    bool CanCraftItem(FName ItemName) const;
+
+    UFUNCTION(BlueprintCallable, Category = "Crafting")
+    bool CraftItem(FName ItemName);
+
+    UFUNCTION(BlueprintCallable, Category = "Combat")
+    void Attack();
+
+    float LastAttackTime;
+    bool bCanAttack;
+
+    // Movement input handlers
+    void MoveUp(const FInputActionValue& Value);
+    void MoveRight(const FInputActionValue& Value);
+    void ZoomCamera(const FInputActionValue& Value);
 };
